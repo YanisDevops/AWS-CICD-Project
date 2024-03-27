d75b4cff861b461735c72490ca0519e6e3a90ba9
YanisDevops <142588782+YanisDevops@users.noreply.github.com>
2024-03-27 10:18:37 +0100

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 set -e
 
+# Get the ID of the running container
+container_id=$(docker ps -q)
+
+# Check if the container ID is not empty
+if [ -n "$container_id" ]; then
+    echo "Running container ID: $container_id"
+    
+    # Stop the container
+    echo "Stopping container..."
+    docker stop $container_id
+    docker rm $container_id
+else
+    echo "No running containers found."
+fi
+
 # Pull the Docker image from Docker Hub
 docker pull yanishub1/hotel-app:latest
 

598446772518128deb4232069d4dd8f814261cdb
YanisDevops <142588782+YanisDevops@users.noreply.github.com>
2024-03-27 01:14:48 +0100

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,1 +1,8 @@
-s
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull yanishub1/hotel-app:latest
+
+# Run the Docker image as a container
+docker run -dit -p 80:80 yanishub1/hotel-app
